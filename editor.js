323353783591c3fb8ac84f0730a21fc7f81094db
joker876 <42680429+joker876@users.noreply.github.com>
2021-03-22 15:17:42 +0100

    removed old code

--- editor.js
+++ editor.js
@@ -89,35 +89,6 @@ var editor_desc = CodeMirror.fromTextArea(document.getElementById('editor-desc')
     mode: "colorCodes",
     simpleScrollbar: true,
 });
-/* 
-CodeMirror.defineMode("strings", function () {
-    return {
-        startState: function () {
-            return {
-                inString: false
-            };
-        },
-        token: function (stream, state) {
-            // If a string starts here
-            if (!state.inString && stream.peek() == '"') {
-                stream.next(); // Skip quote
-                state.inString = true; // Update state
-            }
-            if (state.inString) {
-                if (stream.skipTo('"')) { // Quote found on this line
-                    stream.next(); // Skip quote
-                    state.inString = false; // Clear flag
-                } else {
-                    stream.skipToEnd(); // Rest of line is string
-                }
-                return "string"; // Token style
-            } else {
-                stream.skipTo('"') || stream.skipToEnd();
-                return null; // Unstyled token
-            }
-        }
-    };
-}); 
 
 var editorIn = document.getElementById("editor-name");
 editorIn.addEventListener('keypress', (e) => {
