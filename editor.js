af322449e8c4d7f7a5bdb6622cf8d6de0585c358
joker876 <joker876x@gmail.com>
2023-12-06 20:43:24 +0100

    improve editor

--- editor.js
+++ editor.js
@@ -1,6 +1,6 @@
 CodeMirror.defineMode("colorCodes", function () {
-    let states = ['color', 'matrix', 'bold', 'strikethrough', 'underline', 'italic', 'special']
-    let codes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'k', 'l', 'm', 'n', 'o', 'r'];
+    const states = ['color', 'matrix', 'bold', 'strikethrough', 'underline', 'italic', 'special']
+    const codes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'k', 'l', 'm', 'n', 'o', 'r'];
     return {
         startState: () => {
             return {
@@ -17,34 +17,38 @@ CodeMirror.defineMode("colorCodes", function () {
             // check for the formatting codes
             if (stream.peek() == '&') {
                 stream.next();
-                if (stream.peek() == '0') state[states[0]] = 'black';
-                else if (stream.peek() == '1') state[states[0]] = 'dark_blue';
-                else if (stream.peek() == '2') state[states[0]] = 'dark_green';
-                else if (stream.peek() == '3') state[states[0]] = 'dark_aqua';
-                else if (stream.peek() == '4') state[states[0]] = 'dark_red';
-                else if (stream.peek() == '5') state[states[0]] = 'dark_purple';
-                else if (stream.peek() == '6') state[states[0]] = 'gold';
-                else if (stream.peek() == '7') state[states[0]] = 'gray';
-                else if (stream.peek() == '8') state[states[0]] = 'dark_gray';
-                else if (stream.peek() == '9') state[states[0]] = 'blue';
-                else if (stream.peek() == 'a') state[states[0]] = 'green';
-                else if (stream.peek() == 'b') state[states[0]] = 'aqua';
-                else if (stream.peek() == 'c') state[states[0]] = 'red';
-                else if (stream.peek() == 'd') state[states[0]] = 'light_purple';
-                else if (stream.peek() == 'e') state[states[0]] = 'yellow';
-                else if (stream.peek() == 'f') state[states[0]] = 'white';
-                else if (stream.peek() == 'k') state[states[1]] = true;
-                else if (stream.peek() == 'l') state[states[2]] = true;
-                else if (stream.peek() == 'm') state[states[3]] = true;
-                else if (stream.peek() == 'n') state[states[4]] = true;
-                else if (stream.peek() == 'o') state[states[5]] = true;
-                else if (stream.peek() == 'r') {
-                    state[states[0]] = null;
-                    state[states[1]] = false;
-                    state[states[2]] = false;
-                    state[states[3]] = false;
-                    state[states[4]] = false;
-                    state[states[5]] = false;
+                switch (stream.peek()) {
+
+                    case '0': state[states[0]] = 'black'; break;
+                    case '1': state[states[0]] = 'dark_blue'; break;
+                    case '2': state[states[0]] = 'dark_green'; break;
+                    case '3': state[states[0]] = 'dark_aqua'; break;
+                    case '4': state[states[0]] = 'dark_red'; break;
+                    case '5': state[states[0]] = 'dark_purple'; break;
+                    case '6': state[states[0]] = 'gold'; break;
+                    case '7': state[states[0]] = 'gray'; break;
+                    case '8': state[states[0]] = 'dark_gray'; break;
+                    case '9': state[states[0]] = 'blue'; break;
+                    case 'a': state[states[0]] = 'green'; break;
+                    case 'b': state[states[0]] = 'aqua'; break;
+                    case 'c': state[states[0]] = 'red'; break;
+                    case 'd': state[states[0]] = 'light_purple'; break;
+                    case 'e': state[states[0]] = 'yellow'; break;
+                    case 'f': state[states[0]] = 'white'; break;
+                    case 'k': state[states[1]] = true; break;
+                    case 'l': state[states[2]] = true; break;
+                    case 'm': state[states[3]] = true; break;
+                    case 'n': state[states[4]] = true; break;
+                    case 'o': state[states[5]] = true; break;
+                    case 'r': {
+                        state[states[0]] = null;
+                        state[states[1]] = false;
+                        state[states[2]] = false;
+                        state[states[3]] = false;
+                        state[states[4]] = false;
+                        state[states[5]] = false;
+                        break;
+                    }
                 }
             } else stream.next();
             // ============ mark special characters
@@ -65,7 +69,7 @@ CodeMirror.defineMode("colorCodes", function () {
                 if (codes.includes(stream.peek()) && state.special == 1) state.special = 2;
                 else state.special = 0;
             }
-            
+
             //parse classes
             let ret = '';
             states.forEach(s => {
@@ -92,5 +96,5 @@ var editor_desc = CodeMirror.fromTextArea(document.getElementById('editor-desc')
 
 var editorIn = document.getElementById("editor-name");
 editorIn.addEventListener('keypress', (e) => {
-    if (e.keyCode == 13) e.preventDefault();
+    if (e.key == 'Enter') e.preventDefault();
 });
